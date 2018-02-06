2c0aa01a710f52c2b7f9460431f7ef5cdd072601
YunshanJiang <yunshantree@163.com>
2018-02-06 00:55:56 -0500

    1

---
+++ Source/Game3121_Project/Game.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+class Game : private sf::NonCopyable {
+private:
+	sf::RenderWindow mWindow;
+	sf::CircleShape mPlayer;
+	void processEvents();
+	void render();
+public:
+	Game();
+	void run();
+};
